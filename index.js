5cbd0571f10a70b75f707b258f20519402d972f8
TylerBrinkman <tyler.brinkman@kibocommerce.com>
2023-04-13 16:19:31 -0500

    Update for env Docker run

--- index.js
+++ index.js
@@ -1,26 +1,39 @@
-const {getTenantName, generateImportZip} = require('./import_setup/index')
-const importJob = require('./import_product_data/import')
-const loadSiteData = require('./update_tenant_data/loadSiteData')
-const indexCatalog = require('./update_tenant_data/indexCatalog')
+
+const fs = require('fs')
 
 async function main(){
+	const config = {
+		"appKey": process.env.appKey,
+		"sharedSecret": process.env.sharedSecret,
+		"tenant": process.env.tenant,
+		"site": process.env.site,
+		"baseUrl": "https://home.mozu.com"
+	} 
+
+	fs.writeFileSync('mozu.config.json', JSON.stringify(config), 'utf-8', err => console.log(err))
+
+	const {getTenantName, generateImportZip} = require('./import_setup/index')
+	const importJob = require('./import_product_data/import')
+	const loadSiteData = require('./update_tenant_data/loadSiteData')
+	const indexCatalog = require('./update_tenant_data/indexCatalog')
+	
 	const name = await getTenantName()
 
 	console.log(`Loading catalog data into ${name}`)
 
-	//await generateImportZip(name)
+	await generateImportZip(name)
 
 
 	console.log('Import files generated.....')
 
-	//await importJob()
+	await importJob()
 	console.log('Product data loaded....')
 
-	//await loadSiteData()
+	await loadSiteData()
 
 	console.log('Site data loaded....')
 	console.log('Indexing catalog....')
-	//await indexCatalog(name)
+	await indexCatalog(name)
 
 	console.log('Catalog Indexed....')
 
