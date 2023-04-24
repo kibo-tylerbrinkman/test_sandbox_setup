a6ebb37b846c8e27886a804b17f3e80c658067bb
TylerBrinkman <tyler.brinkman@kibocommerce.com>
2023-04-24 10:22:04 -0500

    Update async code for import

--- import_setup/index.js
+++ import_setup/index.js
@@ -2,6 +2,7 @@ const client = require('mozu-node-sdk/clients/platform/application')()
 const tenantHelper = require('mozu-node-sdk/clients/platform/tenant')(client)
 const csvtojson = require('csvtojson')
 const converter = require('json-2-csv')
+const {wait} = require('../import_product_data/helpers')
 let JSZip = require('jszip')
 fs = require('fs')
 
@@ -9,16 +10,47 @@ async function getTenantName(){
 	const tenantId = client.context.tenant
 	try{
 		const tenantInfo = await tenantHelper.getTenant({tenantId})
-		return tenantInfo.name
+		
+		return tenantInfo.masterCatalogs[1].name
 	}catch(e){
 		console.log(e)
 	}
 
 }
 
+// async function updateCatalogName(file, name){
+// 	console.log('IN UPDATE CATALOG NAME', file)
+// 	const file_json = await csvtojson().fromFile(`./mystic_catalog/product_data/${file}`)
+
+// 	const updated = file_json.map(f => {
+// 		if(f.hasOwnProperty('MasterCatalogName')){
+// 			f.MasterCatalogName = name
+// 		}
+
+// 		if(f.hasOwnProperty('CatalogName')){
+// 			f.CatalogName = name
+// 		}
+// 		return f
+// 	})
+
+// 	converter.json2csv(updated, (err, csv) => {
+// 		if(err){
+// 			console.log(err)
+// 		}
+// 		fs.writeFileSync(`./tmp/${file}`, csv, 'utf-8', err => {
+// 			console.log('ERRR', file)
+// 			console.log(err)
+// 		})
+// 		console.log('wrote file - ', file)
+// 	})
+
+
+
+
+// }
+
 async function updateCatalogName(file, name){
 	const file_json = await csvtojson().fromFile(`./mystic_catalog/product_data/${file}`)
-
 	const updated = file_json.map(f => {
 		if(f.hasOwnProperty('MasterCatalogName')){
 			f.MasterCatalogName = name
@@ -29,27 +61,45 @@ async function updateCatalogName(file, name){
 		}
 		return f
 	})
-
-	converter.json2csv(updated, (err, csv) => {
-		if(err){
-			console.log(err)
-		}
-		fs.writeFileSync(`./tmp/${file}`, csv, 'utf-8')
-		console.log('wrote file')
-	})
-
+    try {
+        await new Promise((resolve, reject) => {
+            converter.json2csv(updated, (err, csv) => {
+                if(err){
+                    console.log(err)
+                    reject(err)
+                }
+                fs.writeFile(`./tmp/${file}`, csv, 'utf-8', err => {
+                    if (err) {
+                        console.log('ERRR', file)
+                        console.log(err)
+                        return reject(err)
+                    }
+                    console.log('wrote file - ', file)
+                    resolve()
+                })
+            })
+        })
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 async function updateImportFiles(name) {
 	const files = await fs.readdirSync('./mystic_catalog/product_data')
 
+	console.log(files)
+
 	if(!fs.existsSync('./tmp')){
 		await fs.mkdirSync('./tmp')
 	}
 
+	const store = []
+
 	for(let file of files){
-		await updateCatalogName(file,name)
+		store.push(updateCatalogName(file,name))
 	}
+
+	await Promise.all(store)
 }
 
 async function generateImportZip(name){
@@ -57,10 +107,15 @@ async function generateImportZip(name){
 	console.log('Updated import files')
 
 	const updated = fs.readdirSync('./tmp')
-
+	
 	const zip = new JSZip()
 	for(let file of updated){
-		zip.file(file, fs.readFileSync(`./tmp/${file}`))
+		try{
+			zip.file(file, fs.readFileSync(`./tmp/${file}`))
+			console.log(`zipping ${file}`)
+		} catch(e){
+			console.log(e)
+		}
 	}
 
 	await zip.generateAsync({ type: "nodebuffer" })
@@ -69,7 +124,10 @@ async function generateImportZip(name){
 			if (err) throw err;
 			console.log("File saved!");
 			});
-  	});
+  	}).catch(function(e){
+		console.log(e)
+	})
+
 }
 
 module.exports = {getTenantName, generateImportZip}
--- index.js
+++ index.js
@@ -2,40 +2,39 @@
 const fs = require('fs')
 
 async function main(){
-	const config = {
-		"appKey": process.env.appKey,
-		"sharedSecret": process.env.sharedSecret,
-		"tenant": process.env.tenant,
-		"site": process.env.site,
-		"baseUrl": "https://home.mozu.com"
-	} 
+	// const config = {
+	// 	"appKey": process.env.appKey,
+	// 	"sharedSecret": process.env.sharedSecret,
+	// 	"tenant": process.env.tenant,
+	// 	"site": process.env.site,
+	// 	"baseUrl": "https://home.mozu.com"
+	// } 
 
-	fs.writeFileSync('mozu.config.json', JSON.stringify(config), 'utf-8', err => console.log(err))
+	// fs.writeFileSync('mozu.config.json', JSON.stringify(config), 'utf-8', err => console.log(err))
 
 	const {getTenantName, generateImportZip} = require('./import_setup/index')
 	const importJob = require('./import_product_data/import')
 	const loadSiteData = require('./update_tenant_data/loadSiteData')
 	const indexCatalog = require('./update_tenant_data/indexCatalog')
 	
-	const name = await getTenantName()
+	// const name = await getTenantName() 
+	const name = 'test'
 
 	console.log(`Loading catalog data into ${name}`)
 
 	await generateImportZip(name)
-
-
 	console.log('Import files generated.....')
 
-	await importJob()
-	console.log('Product data loaded....')
+	// await importJob()
+	// console.log('Product data loaded....')
 
-	await loadSiteData()
+	// await loadSiteData()
 
-	console.log('Site data loaded....')
-	console.log('Indexing catalog....')
-	await indexCatalog(name)
+	// console.log('Site data loaded....')
+	// console.log('Indexing catalog....')
+	// await indexCatalog(name)
 
-	console.log('Catalog Indexed....')
+	// console.log('Catalog Indexed....')
 
 	//images......
 }
